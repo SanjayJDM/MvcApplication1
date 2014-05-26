ab2c8b14746152d77adced1a7e9e568bb7a0913c
Sanjay <sanjayjdm@gmail.com>
2014-05-26 23:42:09 +0100

    added new files to get and post

---
+++ MvcApplication1/Controllers/GetLocationController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace MvcApplication1.Controllers
+{
+    public class GetLocationController : ApiController
+    {
+        // GET api/getlocation
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/getlocation/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+       
+    }
+}
---
+++ MvcApplication1/Controllers/PostLocationController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace MvcApplication1.Controllers
+{
+    public class PostLocationController : ApiController
+    {
+      
+        // POST api/postlocation
+        public void Post(string appKey,string assetId,string lat, string lon, [FromBody]string value)
+        {
+            string[] geoCordinates;
+            if (appKey == "ttpapikey.asxc123nju89mno0")
+            {
+                if (value != null)
+                {
+                    geoCordinates = value.Split(',');
+                }
+
+
+            }
+        }
+        
+        // PUT api/postlocation/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/postlocation/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
