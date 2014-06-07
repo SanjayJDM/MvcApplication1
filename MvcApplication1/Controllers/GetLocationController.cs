c0ce7acfc3e566b3b7be24704afdadc1396dc8d7
Sanjay <sanjayjdm@gmail.com>
2014-06-07 01:00:25 +0100

    Updated with GetLocation API - BUILD WORKING

--- MvcApplication1/Controllers/GetLocationController.cs
+++ MvcApplication1/Controllers/GetLocationController.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
@@ -10,9 +12,49 @@ namespace MvcApplication1.Controllers
     public class GetLocationController : ApiController
     {
         // GET api/getlocation
-        public IEnumerable<string> Get()
+        public IEnumerable<string> Get(string appKey, string assetId)
         {
-            return new string[] { "value1", "value2" };
+            SqlConnection con = new SqlConnection(ConfigurationManager.AppSettings["SqlConString"]);
+            SqlCommand cmd;
+            SqlDataReader reader = null;
+            if (appKey == "ttpapikey.asxc123nju89mno0")
+            {
+                try
+                {
+                    con.Open();
+                    try
+                    {
+                        cmd = new SqlCommand("SELECT [latitude],[longitude] FROM [dbo].[AssetLocationDet] WHERE [assetId]="+assetId, con);
+                        reader = cmd.ExecuteReader();
+                        while (reader.Read())
+                        {
+                           // reader["lat"].ToString();
+                            //reader["lon"].ToString();
+                            return new string[] { "lat:" + reader["latitude"].ToString(), "lon:" + reader["longitude"].ToString() };
+                        }
+
+                        
+                        
+                    }
+                    catch (Exception ex)
+                    {
+                        throw ex.InnerException;
+                    }
+                    finally
+                    {
+                        if (con.State == System.Data.ConnectionState.Open)
+                        {
+                            con.Close();
+                        }
+                    }
+                }
+                catch (Exception e)
+                {
+                    throw e;
+                }
+                
+            }
+            return new string[] { "lat:0", "lon:0" };
         }
 
         // GET api/getlocation/5
