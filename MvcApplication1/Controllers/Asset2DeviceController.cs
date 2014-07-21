357cd460e329ea9835161622c675034dc0ca8d80
Sanjay <sanjayjdm@gmail.com>
2014-07-21 22:46:08 +0100

    Added Asset2Device and Get AssetID controllers

---
+++ MvcApplication1/Controllers/Asset2DeviceController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace MvcApplication1.Controllers
+{
+    public class Asset2DeviceController : ApiController
+    {
+        public void Post(string appKey, string assetid, string deviceid, [FromBody]string value)
+        {
+            SqlConnection con = new SqlConnection(ConfigurationManager.AppSettings["SqlConString"]);
+            SqlCommand cmd;
+            // DateTime currentDateTime = new DateTime();
+            if (appKey == "ttpapikey.asxc123nju89mno0")
+            {
+                try
+                {
+                    con.Open();
+                    try
+                    {
+                        //cmd = con.CreateCommand();
+
+                        {
+                            cmd = new SqlCommand("sp_AssetDeviceMap", con);
+                            //cmd = new SqlCommand("INSERT INTO AssetLocationDet (assetId,latitude,longitude) VALUES (" + assetId + "," + lat + "," + lon + ")", con);
+                            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                            cmd.Parameters.Add(new SqlParameter("@assetid", assetid));
+                            cmd.Parameters.Add(new SqlParameter("@deviceid", deviceid));
+                            cmd.ExecuteNonQuery();
+                            //cmd.CommandText = "INSERT INTO AssetLocationDet (assetId,latitude,longitude) VALUES (" + assetId + "," + lat + "," + lon + ")";
+                        }
+
+                    }
+                    catch (Exception ex)
+                    {
+                        throw ex.InnerException;
+                    }
+                    finally
+                    {
+                        if (con.State == System.Data.ConnectionState.Open)
+                        {
+                            con.Close();
+                        }
+                    }
+                }
+                catch (Exception e)
+                {
+                    throw e.InnerException;
+                }
+            }
+        }
+    }
+}
---
+++ MvcApplication1/Controllers/GetAssetIdController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace MvcApplication1.Controllers
+{
+    public class GetAssetIdController : ApiController
+    {
+        public string Get(string appKey, string deviceId)
+        {
+            SqlConnection con = new SqlConnection(ConfigurationManager.AppSettings["SqlConString"]);
+            SqlCommand cmd;
+            SqlDataReader reader = null;
+            string jsonStringConstructor = string.Empty;
+            if (appKey == "ttpapikey.asxc123nju89mno0")
+            {
+                try
+                {
+                    con.Open();
+                    try
+                    {
+                        cmd = new SqlCommand("SELECT AssetId FROM [dbo.AssetDeviceMap] WHERE [DeviceId]=" + deviceId, con);
+                        reader = cmd.ExecuteReader();
+                        while (reader.Read())
+                        {
+                            // reader["lat"].ToString();
+                            //reader["lon"].ToString();
+                            return  "AssetId:" + reader["AssetId"].ToString();
+                        }
+
+                    }
+                    catch (Exception ex)
+                    {
+                        throw ex.InnerException;
+                        return "Error Occured";
+                    }
+                    finally
+                    {
+                        if (con.State == System.Data.ConnectionState.Open)
+                        {
+                            con.Close();
+                        }
+                    }
+                }
+                catch (Exception e)
+                {
+                    throw e;
+                    return "Error Occured";
+                }
+
+                return "No AssetID found";
+
+            }
+        }
+    }
+}
